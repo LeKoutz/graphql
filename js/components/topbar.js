846813ddf1ce78a2d8f28ebd101289bfff9b43f6
Georgios Koutzos <koutzosgiorgos@gmail.com>
2026-07-26 10:41:05 +0300

    Adds Platform and Gitea redirection buttons on topbar

--- js/components/topbar.js
+++ js/components/topbar.js
@@ -7,23 +7,25 @@ export function createTopBar(user) {
     const welcome = document.createElement('h2');
     welcome.textContent = `Welcome, ${user.login}`;
 
-    /*const info = document.createElement('button')
-    info.classList.add('topBar-btn')
-    info.textContent = 'Info'
+    const platform = document.createElement('a');
+    platform.id = 'platform-button';
+    platform.classList.add('topBar-btn');
+    platform.href = `https://platform.zone01.gr/intra/`;
+    platform.target = '_blank';
+    platform.textContent = 'Zone01';
 
-    const audits = document.createElement('button')
-    audits.classList.add('topBar-btn')
-    audits.textContent = 'Audits'
-
-    const progress = document.createElement('button')
-    progress.classList.add('topBar-btn')
-    progress.textContent = 'Progress'*/
+    const gitea = document.createElement('a');
+    gitea.id = 'gitea-button';
+    gitea.classList.add('topBar-btn');
+    gitea.href = `https://platform.zone01.gr/git/${user.login}`;
+    gitea.target = '_blank';
+    gitea.textContent = 'Gitea';
 
     const logoutBtn = document.createElement('button');
     logoutBtn.classList.add('topBar-btn');
     logoutBtn.textContent = 'Log out';
     logoutBtn.addEventListener('click', logout);
 
-    topBar.append(welcome, logoutBtn);
+    topBar.append(welcome, platform, gitea, logoutBtn);
     return topBar;
 }
